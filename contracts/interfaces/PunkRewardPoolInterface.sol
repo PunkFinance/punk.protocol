36cf0af128de4ac409dc061f56f30429e68446cc
0xViktor <viktor@punk.finance>
2021-05-28 19:09:30 +0900

    Hard Work Now! For Punkers by 0xViktor...
    - Update Punk Reward Pool Interface

--- contracts/interfaces/PunkRewardPoolInterface.sol
+++ contracts/interfaces/PunkRewardPoolInterface.sol
@@ -3,17 +3,24 @@ pragma solidity >=0.5.0 <0.9.0;
 
 interface PunkRewardPoolInterface {
     function initializePunkReward( address storage_, address punk_ ) external;
+
     function addForge( address forge ) external;
     function setForge( address forge, uint weight ) external;
-    function checkForge( address forge ) external view returns( bool );
-    function getMinWeight() external view returns( uint );
-    function claimPunk() external;
+    function getWeightRange( address forge ) external view returns( uint, uint );
+
+    function claimPunk( ) external;
     function claimPunk( address to ) external;
     function claimPunk( address forge, address to ) external;
     function staking( address forge, uint amount ) external;
     function unstaking( address forge, uint amount ) external;
+    
     function getClaimPunk( address to ) external view returns( uint );
     function getClaimPunk( address forge, address to ) external view returns( uint );
+    
     function getWeightSum() external view returns( uint );
     function getWeight( address forge ) external view returns( uint );
+    function getTotalDistributed( ) external view returns( uint );
+    function getDistributed( address forge ) external view returns( uint );
+    function getAllocation( ) external view returns( uint );
+    function getAllocation( address forge ) external view returns( uint );
 }
