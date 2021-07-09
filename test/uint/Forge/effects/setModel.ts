c096f0de0035f9618fe76c6485558ee23408a040
Khanh <duykhanh9242@gmail.com>
2021-07-09 11:39:26 +1000

    test setModel method with non-contract address

--- test/uint/Forge/effects/setModel.ts
+++ test/uint/Forge/effects/setModel.ts
@@ -2,15 +2,17 @@ import { BigNumber } from "@ethersproject/bignumber"
 import { Zero } from "@ethersproject/constants"
 import { expect } from "chai"
 
+import { ForgeErrors } from '../../../shared/errors'
+
 export default function shouldBehaveLikeSetModel(): void {
     context("when setModel called with contract address", function() {
         beforeEach(async function() {
         })
         
-        it("setModel", async function() {
-            console.log('result', this.signers.account0.address)
-            const result: boolean = await this.contracts.forge.connect(this.signers.account0).setModel(this.signers.account0.address)
-            expect(result).to.be.true;
+        it("setModel with non-contract address", async function() {
+            await expect(
+                this.contracts.forge.connect(this.signers.account0).setModel(this.signers.account0.address),
+            ).to.be.revertedWith(ForgeErrors.NotContract)
         })
     })
 }
